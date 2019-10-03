1b73e3c7d4dd751e7349336174ab624d3c77a8c1
Anthony Mattas <amattas@MacBook-Pro.mattas.net>
2019-10-03 18:26:39 -0400

    Update cloudstorage.py
    Added retry logic for Azure file upload

--- cloudstorage.py
+++ cloudstorage.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 import os
+import azure.common
 from azure.storage.blob import BlockBlobService
 
 
@@ -25,17 +26,23 @@ class CloudStorage:
         self.logger.debug("Leaving store_cloud_image()")
 
     def _store_azure_blob(self, filename):
-        self.logger.debug("Entering _store_azure_blob()")
-        self.logger.debug("Creating BlobServiceClient")
-        storage_service = BlockBlobService(account_name=self._config.azure_storage_account,
-                                           account_key=self._config.azure_storage_key)
-        self.logger.debug("Writing File %s", filename)
-        # storage_container =  storage_service.acquire_container_lease(self._azure_storage_container)
-        self.logger.debug("Getting BlobClient %s", filename)
-        storage_service.create_blob_from_path(container_name=self._config.azure_storage_container,
-                                              blob_name=filename,
-                                              file_path=self._config.storage_path + filename)
-        if self._config.storage_delete_on_upload:
-            self.logger.debug("Deleting file %s", filename)
-            os.remove(self._config.storage_path + filename)
+        for retry_counter in range(5):
+            try:
+                self.logger.debug("Entering _store_azure_blob()")
+                self.logger.debug("Creating BlobServiceClient")
+                storage_service = BlockBlobService(account_name=self._config.azure_storage_account,
+                                                   account_key=self._config.azure_storage_key)
+                self.logger.debug("Writing File %s", filename)
+                # storage_container =  storage_service.acquire_container_lease(self._azure_storage_container)
+                self.logger.debug("Getting BlobClient %s", filename)
+                storage_service.create_blob_from_path(container_name=self._config.azure_storage_container,
+                                                      blob_name=filename,
+                                                      file_path=self._config.storage_path + filename)
+                if self._config.storage_delete_on_upload:
+                    self.logger.debug("Deleting file %s", filename)
+                    os.remove(self._config.storage_path + filename)
+                    break
+            except azure.common.AzureException:
+                self.logger.info("Upload to Azure Storage failed attempt %s", retry_counter+1)
+                continue
         self.logger.debug("Leaving _store_azure_blob()")
