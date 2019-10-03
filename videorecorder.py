2a7d831775699cc863307876645d1cdda526570d
Anthony Mattas <amattas@MacBook-Pro.mattas.net>
2019-10-03 15:14:19 -0400

    Update videorecorder.py

--- videorecorder.py
+++ videorecorder.py
@@ -71,6 +71,7 @@ class VideoRecorder:
         self._current_video_filename = str(uuid.uuid4().hex) + self._config.camera_file_extension
         self.logger.debug("Video file name is %s", self._current_video_filename)
         self.logger.debug("Creating video writer")
+        video_codec = cv2.VideoWriter_fourcc(*self._config.camera_fourcc_codec)
         self._video_writer = cv2.VideoWriter(self._config.storage_path+self._current_video_filename, video_codec,
                                        self._config.camera_framerate,
                                        (self._config.camera_xresolution, self._config.camera_yresolution))
@@ -79,7 +80,6 @@ class VideoRecorder:
         video_capture = cv2.VideoCapture(0)
         video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, self._config.camera_xresolution)
         video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self._config.camera_yresolution)
-        video_codec = cv2.VideoWriter_fourcc(*self._config.camera_fourcc_codec)
         self.logger.debug("Writing file %s", self._current_video_filename)
         while self._recording:
             ret, frame = video_capture.read()
