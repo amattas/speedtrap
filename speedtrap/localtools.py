ec7a31af18d387b1e9890e5ae6016019c09dbed3
amattas <anthony@mattas.net>
2020-06-10 08:41:00 -0400

    localtools.py - Addition of Python docstrings to address https://github.com/amattas/speedtrap/issues/12

--- speedtrap/localtools.py
+++ speedtrap/localtools.py
@@ -3,14 +3,42 @@ import logging
 
 
 class LocalTools:
+    """
+    This class is a helper class used to provide simple helper methods on the local system.
+    """
 
     def __init__(self, config):
+        """
+        This is the constructor used for class initialization
+
+        Parameters
+        ----------
+        config : Configuration
+            This is a populated instances of the Configuration class with all of the settings loaded fom the specified
+            configuration file
+
+        Returns
+        ------
+        LocalTools:
+            Returns a LocalTools instance initialized with the provided Configuration.
+        -----
+        """
         self._config = config
         logging.basicConfig(level=self._config.logging_level)
         self.logger = logging.getLogger('SpeedTrap.LocalTools')
 
     @staticmethod
     def clean_local(config):
+        """
+        This method is used to clean up local files on the local filesystem. It uses a instance of the
+        Configuration class to determine which files need to be removed.
+
+        Parameters
+        ----------
+        config : Configuration
+            This is a populated instances of the Configuration class with all of the settings loaded fom the specified
+            configuration file
+        """
         logging.basicConfig(level=config.logging_level)
         logger = logging.getLogger('SpeedTrap.LocalTools')
         logger.debug("Entering clean_local()")
