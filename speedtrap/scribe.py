e041d5adc824008077252854589800fb2402db69
amattas <anthony@mattas.net>
2020-06-10 11:37:38 -0400

    scribe.py - Enhancing logging capabilities for issue https://github.com/amattas/speedtrap/issues/22

--- speedtrap/scribe.py
+++ speedtrap/scribe.py
@@ -31,8 +31,9 @@ class Scribe:
         """
         self._config = config
         logging.basicConfig(level=self._config.logging_level)
-        self.logger = logging.getLogger('SpeedTrap.DataRecorder')
-        self.logger.debug("Creating DataRecorder() instance")
+        self.logger = logging.getLogger('SpeedTrap.Scribe')
+        self.logger.debug("Creating Scribe() instance")
+        self.logger.debug("Setting up ODBCDatabase() instance")
         self._odbc_database = ODBCDatabase(config)
 
     def capture(self, data_child):
@@ -48,16 +49,19 @@ class Scribe:
             also receive a SpeedRecord object instance. When receiving a SpeedRecord object is passes it on and calls
             the private _save_record method.
         """
-        data_result = 0
+        self.logger.debug("Entering capture()")
         while True:
             if data_child.poll():
+                self.logger.debug("Message received on data_child Pipe()")
                 data_result = data_child.recv()
                 if type(data_result) is int:
                     if data_result == -1:
-                        self.logger.debug("Received -1 termination message")
+                        self.logger.debug("Mode now set to -1")
                         break
                 elif type(data_result) is SpeedRecord:
+                    self.logger.debug("Message was new SpeedRecord")
                     self._save_record(data_result.get_speed(), data_result.get_time(), data_result.get_filename())
+        self.logger.debug("Leaving capture()")
 
     def _save_record(self, speed, recorded_time, filename):
         """
@@ -73,18 +77,21 @@ class Scribe:
         filename: str
             This is the name of the file provided in the SpeedRecord
         """
-        self.logger.debug("Entering record()")
+        self.logger.debug("Entering _save_record()")
         if self._config.enable_local_database:
+            self.logger.debug("Local database saving is enabled")
             self._record_storage(speed, recorded_time, filename)
         if self._config.enable_azure:
+            self.logger.debug("Azure storage saving is enabled")
             cloud_storage = CloudStorage(self._config)
             cloud_storage.store_cloud_image(filename)
             if self._config.enable_odbc:
+                self.logger.debug("ODBC database saving is enabled")
                 self._odbc_database.database_record(speed, recorded_time, self._config.azure_storage_uri_prefix + filename)
         elif self._config.enable_odbc:
+            self.logger.debug("ODBC database saving is enabled")
             self._odbc_database.database_record(speed, recorded_time, filename)
-
-        self.logger.debug("Leaving record()")
+        self.logger.debug("Leaving _save_record()")
 
     def _record_storage(self, speed, recorded_time, filename):
         """
@@ -105,5 +112,6 @@ class Scribe:
         csv_file = open(csv_filename,'a')
         csv_writer = csv.writer(csv_file, dialect='excel')
         csv_writer.writerows(record)
+        self.logger.info("Successfully saved %s to local storage", filename)
         csv_file.close()
-        self.logger.debug("Entering _record_storage()")
+        self.logger.debug("Leaving _record_storage()")
