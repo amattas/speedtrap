1d97646f5865012c8b90995f84295e0366e70788
amattas <anthony@mattas.net>
2020-06-10 09:42:10 -0400

    scribe.py - Added Python docstrings to resolve https://github.com/amattas/speedtrap/issues/15

--- speedtrap/scribe.py
+++ speedtrap/scribe.py
@@ -9,11 +9,26 @@ from speedtrap.odbcdatabase import ODBCDatabase
 # ToDo: Document Class
 class Scribe:
     """
-    The DataRecorder class is designed to take a SpeedRecord and create a log entry either locally or on cloud
-    storage
+    The Scribe class is designed to take a SpeedRecord and and handle down stream "data" management, including writing
+    the record out locally to CSV or to a database, and managing any necessary file uploads.
     """
 
     def __init__(self, config):
+        """
+        This is the constructor used for class initialization
+
+        Parameters
+        ----------
+        config : Configuration
+            This is a populated instance of the Configuration class with all of the settings loaded fom the specified
+            configuration file
+
+        Returns
+        ------
+        Scribe:
+            Returns a Scribe instance initialized with the provided Configuration.
+        -----
+        """
         self._config = config
         logging.basicConfig(level=self._config.logging_level)
         self.logger = logging.getLogger('SpeedTrap.DataRecorder')
@@ -21,6 +36,18 @@ class Scribe:
         self._odbc_database = ODBCDatabase(config)
 
     def capture(self, data_child):
+        """
+        Listens for messages arriving on a multiprocessing.Pipe() and takes the necessary actions. This method is
+        intended to be called as its own multiprocessing.Process()
+
+        Parameters
+        ----------
+        data_child : Pipe()
+            This is one half of a bidirectional multiprocessing.Pipe(). It's used to control the behavior of the
+            method. Possible values include: -1 which will cause the process to clean-up and exit gracefully or it can
+            also receive a SpeedRecord object instance. When receiving a SpeedRecord object is passes it on and calls
+            the private _save_record method.
+        """
         data_result = 0
         while True:
             if data_child.poll():
@@ -33,6 +60,19 @@ class Scribe:
                     self._save_record(data_result.get_speed(), data_result.get_time(), data_result.get_filename())
 
     def _save_record(self, speed, recorded_time, filename):
+        """
+        This private method processes the information contained in a SpeedRecord and dispatches it based on in the
+        Configuration instance provided with the calss was instantiated.
+
+        Parameters
+        ----------
+        speed : str
+            This is the speed of the event provided in the SpeedRecord
+        recorded_time: time
+            This is the time of the event provided in the SpeedRecord
+        filename: str
+            This is the name of the file provided in the SpeedRecord
+        """
         self.logger.debug("Entering record()")
         if self._config.enable_local_database:
             self._record_storage(speed, recorded_time, filename)
@@ -47,6 +87,18 @@ class Scribe:
         self.logger.debug("Leaving record()")
 
     def _record_storage(self, speed, recorded_time, filename):
+        """
+        This private method store a copy of the SpeedRecrod as a CSV in the local filesystem storage
+
+        Parameters
+        ----------
+        speed : str
+            This is the speed of the event to store to local storage
+        recorded_time: time
+            This is the time of the event to store to local st orage
+        filename: str
+            This is the name of the file to store to local storage
+        """
         self.logger.debug("Entering _record_storage()")
         record = [(speed, time.strftime('%Y-%m-%d %H:%M:%S', recorded_time), filename)]
         csv_filename = self._config.database_path + self._config.database_filename
